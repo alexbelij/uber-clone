c0968925de8a78f80754d30c06b3bf9dd7d2150f
guuhx97 <gustavorobertotb@outlook.com>
2020-01-22 22:56:27 -0300

    🚖 Feat: Uber clone application finalized

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,16 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
-import MapView from 'react-native-maps'
+import React, { useEffect, useState, useRef, Fragment } from 'react';
+import { View, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import  Geocoder from 'react-native-geocoding';
 
 import Search from '../Search';
 import Directions from '../Directions';
+import Detais from '../Details';
+import { getPixelSize } from '../../utils';
 
-import styles from './styles';
- 
+import { LocationBox, LocationText, LocationTimeText, LocationTimeTextSmall, LocationTimeBox, Back } from './styles';
+import markerImage from '../../assets/marker.png' 
+import backImage from '../../assets/back.png' 
+
+Geocoder.init('API_KEY');
+
 function Map(){
   const [currentRegion, setCurrentRegion] = useState(null);
   const [destination, setDestination] = useState(null);
+  const [duration, setDuration] = useState(0);
+  const [currentLocation, setCurrentLocation] = useState(null);
+
+  let mapView = useRef(null)
 
   useEffect(() => {
     async function loadInitialPosition(){
@@ -20,6 +31,10 @@ function Map(){
          enableHighAccuracy: false //FALSE WIFI, TRUE GPS
        });
        const { latitude, longitude} = coords;
+       const response = await Geocoder.from({latitude, longitude});
+       const address = response.results[0].formatted_address;
+       setCurrentLocation(address.substring(0, address.indexOf(',')));
+
        setCurrentRegion({
          latitude,
          longitude,
@@ -31,6 +46,10 @@ function Map(){
     loadInitialPosition();
   }, []);
 
+  const handleBack = () => {
+    setDestination(null);
+  }
+
   const handleLocationSelected = (data, { geometry }) => {
     const { location: {lat: latitude, lng: longitude } } = geometry;
     setDestination({
@@ -39,9 +58,6 @@ function Map(){
       title: data.structured_formatting.main_text,
     })
   }
-
-
-
   if(!currentRegion){
     return null;
   }
@@ -49,20 +65,63 @@ function Map(){
   return (
     <View style={{flex: 1}} >
       <MapView 
+        style={{ flex: 1}}
         initialRegion={currentRegion} 
-        style={styles.map}
         showsUserLocation
         loadingEnabled  
+        ref={el => {mapView = el}}
       >
         { destination && (
-          <Directions 
-            origin={currentRegion}
-            destination={destination}
-            onReady={() => {}}
-          />
+          <>
+            <Directions 
+              origin={currentRegion}
+              destination={destination}
+              onReady={result => {
+                mapView.fitToCoordinates(result.coordinates, {
+                  edgePadding: {
+                    right: getPixelSize(50),
+                    left: getPixelSize(50),
+                    top: getPixelSize(50),
+                    bottom: getPixelSize(350),
+                  }
+                });
+                setDuration(Math.floor(result.duration));
+              }}
+              />
+            <Marker 
+              coordinate={destination}
+              anchor={{ x: 0, y: 0 }}
+              image={markerImage}
+            >
+              <LocationBox>
+                <LocationText>{destination.title}</LocationText>
+              </LocationBox>
+            </Marker>
+
+            <Marker 
+              coordinate={currentRegion}
+              anchor={{ x: 0, y: 0 }}
+            >
+              <LocationBox>
+                <LocationTimeBox>
+                  <LocationTimeText>{duration}</LocationTimeText>
+                  <LocationTimeTextSmall>MIN</LocationTimeTextSmall>
+                </LocationTimeBox>
+                <LocationText>{currentLocation}</LocationText>
+              </LocationBox>
+            </Marker>
+          </>
         )}
       </MapView>
-      <Search onLocationSelected={handleLocationSelected} />
+      { destination ? (
+        <Fragment>
+          <Back onPress={handleBack}>
+            <Image source={backImage} />
+          </Back>
+          <Detais />
+        </Fragment>
+      ) : <Search onLocationSelected={handleLocationSelected} />}
+      
     </View>
   )
 }
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 import styles from './styles';
 
 export default function Search({ onLocationSelected }) {
+  const [searchFocused, setSearchFocused] = useState(false);
 
   return (
     <GooglePlacesAutocomplete
@@ -11,13 +12,20 @@ export default function Search({ onLocationSelected }) {
       placeholderTextColor="#333"
       onPress={onLocationSelected}
       query={{
-        key: "API KEY",
+        key: "API_KEY",
         language: "pt"
       }}
       textInputProps={{
+        onFocus: () => {
+          setSearchFocused(true)
+        },
+        onBlur: () => {
+          setSearchFocused(false)
+        },
         autoCapitalize: "none",
         autoCorrect: false
       }}
+      listViewDisplayed={searchFocused}
       fetchDetails
       enablePoweredByContainer={false}
       styles={styles}
